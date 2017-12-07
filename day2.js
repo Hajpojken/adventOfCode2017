23ebeee10ca5d30f99a73e05f256607ca44c855c
Johan Forslund <jforslund89@gmail.com>
2017-12-07 13:14:42 +0100

    Update day2.js

--- day2.js
+++ day2.js
@@ -7,8 +7,8 @@ var a = fs.readFileSync('./text', 'utf-8').split('\n').map(function(x){
   return !isNaN(x[0])
 })
 
-function day2A(arg){
-  return arg.reduce(function(prevVal, x, i, array) {
+function day2A(arr){
+  return arr.reduce(function(prevVal, x, i, array) {
     return prevVal +=
       (array[i].reduce(function(a,b){
         return Math.max(a, b)}) -
@@ -17,8 +17,8 @@ function day2A(arg){
   }, 0);
 }
 
-function day2B(arg){
-  return arg.reduce(function(bigVal, x, i, bigArray){
+function day2B(arr){
+  return arr.reduce(function(bigVal, x, i, bigArray){
     return bigVal + bigArray[i].reduce(function(prevVal, x, j, array){
       return prevVal + array.reduce(function(prevVal, b, k, array){
         return x%b === 0 && k!=j? prevVal + x/b : prevVal
